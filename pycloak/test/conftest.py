00e62adf99f14b6e5a17d47e4c86125c988ae944
Josh Cain <jcain@redhat.com>
2017-12-18 13:35:46 -0600

    constantize and clean up

--- pycloak/test/conftest.py
+++ pycloak/test/conftest.py
@@ -2,16 +2,23 @@
 import pytest
 
 @pytest.fixture(scope='session')
-def keycloak_server(request):
+def admin_username():
+    return 'admin'
+
+@pytest.fixture(scope='session')
+def admin_password():
+    return 'password'
+
+@pytest.fixture(scope='session')
+def keycloak_server(request, admin_username, admin_password):
     print('Starting docker container for Keycloak server...')
     import docker
     import requests
     import time
 
-    # TODO error handling here for if a docker client is not detected
-    # start up docker container
+    docker_instance_name = 'pycloak-test-server'
     client = docker.from_env()
-    client.containers.run('jboss/keycloak:3.4.1.Final', environment={'KEYCLOAK_USER': 'admin', 'KEYCLOAK_PASSWORD': 'password'}, ports={'8080/tcp': '8080'}, name='pycloak-test-server', remove=True, detach=True)
+    client.containers.run('jboss/keycloak:3.4.1.Final', environment={'KEYCLOAK_USER': admin_username, 'KEYCLOAK_PASSWORD': admin_password}, ports={'8080/tcp': '8080'}, name=docker_instance_name, remove=True, detach=True)
 
     booted = False;
     while not booted:
@@ -20,13 +27,14 @@ def keycloak_server(request):
 
         try:
             booted = requests.get('http://127.0.0.1:8080/auth/admin/master/console/').status_code == 200
+            print('Keycloak console is up, fixture is ready.')
         except requests.exceptions.ConnectionError:
             print('Keycloak admin console not available')
 
     def fin():
         print('Stopping docker container for Keycloak server....')
         # spin down docker container
-        pycloak_test_severs = client.containers.list(filters={'name': 'pycloak-test-server'})
+        pycloak_test_severs = client.containers.list(filters={'name': docker_instance_name})
         if pycloak_test_severs:
             pycloak_test_severs[0].stop()
             print('Keycloak server shutdown complete')
--- pycloak/test/test_auth.py
+++ pycloak/test/test_auth.py
@@ -1,13 +1,13 @@
 
-from pycloak import admin, auth
+from pycloak import auth
 import pytest
 
-def test_auth_success(keycloak_server):
-    session = auth.AuthSession('admin', 'password')
+def test_auth_success(keycloak_server, admin_username, admin_password):
+    session = auth.AuthSession(admin_username, admin_password)
     access_token = session.get_access_token()
     assert access_token != None
 
-def test_auth_failure(keycloak_server):
-    session = auth.AuthSession('admin', 'NOTpassword')
+def test_auth_failure(keycloak_server, admin_username, admin_password):
+    session = auth.AuthSession(admin_username, 'NOT' + admin_password)
     with pytest.raises(auth.AuthException, message='Invalid credentials did not cause exception'):
         access_token = session.get_access_token()
