fcfafaa82df7e1dc279662898c0c5a9701f4de2a
Josh Cain <jcain@redhat.com>
2018-01-03 08:01:07 -0600

    bump version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pycloak',
     packages=['pycloak','pycloak.test'],
-    version='0.1.1.snapshot',
+    version='0.1.1',
     description='API for Keycloak Admin Interface',
     author='Josh Cain',
     author_email='jcain@redhat.com',
