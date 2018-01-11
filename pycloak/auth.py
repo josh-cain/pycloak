dde5fbfcc90342f614e3e569e3f3d1549be23d62
Josh Cain <jcain@redhat.com>
2018-01-11 11:02:20 -0600

    add ability to use offline tokens for auth instead of username+password

--- pycloak/auth.py
+++ pycloak/auth.py
@@ -10,31 +10,45 @@ import urllib.request
 logger = logging.getLogger('pycloak.auth')
 
 
-def direct_access_grant_token(username, password, host="http://localhost:8080", realm="master", client_id="admin-cli"):
+def direct_access_grant_token(username, password, host="http://localhost:8080", realm="master", client_id="admin-cli", include_offline=False):
     """
     Makes a request against the Keycloak openid-connect/token endpoint using a direct access grant to get a token.
 
     Returned JSON objects will look like this:
-    {'access_token': 'ey...2w', 'expires_in': 60, 'refresh_expires_in': 1800,
-    'refresh_token': 'ey..Og', 'not-before-policy': 0, 'session_state': '5ca9ccf7-b425-4fe8-81a1-8bbd770ee312', 'token_type': 'bearer'}
+    {'access_token': 'ey...2w', 'expires_in': 60, 'refresh_expires_in': 1800, 'refresh_token': 'ey..Og', 'not-before-policy': 0, 'session_state': '5ca9ccf7-b425-4fe8-81a1-8bbd770ee312', 'token_type': 'bearer'}
 
     @param username: user for which the token is to be granted
     @param password: user's password
     @param host: full hostname with protocol of the keycloak auth server
     @param realm: realm against which the direct access grant will be made
     @param client_id: client argument for the direct access grant request
+    @param include_offline: denotes whether or not to include the parameter for an offline token request.
     @return: json object representing token Response
     @raise: AuthException if a non-200 response is returned by the token endpoint
     """
-    token_request_data = {'grant_type': 'password',
-                          'client_id': client_id, 'username': username, 'password': password}
-    token_request_url = "{0}/auth/realms/{1}/protocol/openid-connect/token".format(
-        host, realm)
+    token_request_data = {'grant_type': 'password', 'client_id': client_id, 'username': username, 'password': password}
+    if include_offline:
+        token_request_data['scope'] = 'offline_access'
+    token_request_url = "{0}/auth/realms/{1}/protocol/openid-connect/token".format(host, realm)
     token_response = requests.post(token_request_url, data=token_request_data)
 
     if token_response.status_code != 200:
-        raise AuthException(
-            'Non-200 Response returned from direct access grant attempt: {}'.format(token_response.status_code))
+        raise AuthException('Non-200 Response returned from direct access grant attempt: {}'.format(token_response.status_code))
+
+    return json.loads(token_response.text)
+
+def offline_token(offline_token , host="http://localhost:8080", realm="master", client_id="admin-cli", include_offline=False):
+    token_request_data = {'grant_type': 'refresh_token', 'client_id': client_id, 'refresh_token': offline_token}
+
+    if include_offline:
+        token_request_data['scope'] = 'offline_access'
+    token_request_url = "{0}/auth/realms/{1}/protocol/openid-connect/token".format(host, realm)
+    token_response = requests.post(token_request_url, data=token_request_data)
+
+    if token_response.status_code == 400:
+        raise AuthException('400/Bad Request returned.  Validate that the client allows this kind of grant and the calling user has the offline_access role (and it is effective for the client)')
+    if token_response.status_code != 200:
+        raise AuthException('Non-200 Response returned from offline token refresh attempt: {}'.format(token_response.status_code))
 
     return json.loads(token_response.text)
 
@@ -58,17 +72,27 @@ class AuthSession:
 
     __access_token = None
 
-    def __init__(self, username, password, host="http://localhost:8080", realm="master", client_id="admin-cli"):
+    def __init__(self, username=None, password=None, offline_token=None, host="http://localhost:8080", realm="master", client_id="admin-cli"):
+        if offline_token is None and (username is None or password is None):
+            raise AuthException("Unable to authenticate, no form of credentials provided.  Please supply either a username/password pair or offline token.")
+
         self.username = username
         self.password = password
+        self.token = offline_token
         self.host = host
         self.realm = realm
         self.client_id = client_id
 
+    #TODO should be using refresh tokens instead of direct access grants for re-auth every time
     def get_access_token(self):
         if self.__access_token is None or is_expired(self.__access_token):
-            logging.debug('Using direct access grant to acquire access token.')
-            token_response = direct_access_grant_token(self.username, self.password, self.host, self.realm, self.client_id)
+            if self.token is not None:
+                logging.debug('Using offline token grant to acquire access token.')
+                token_response = offline_token(self.offline_token, self.host, self.realm, self.client_id)
+            else:
+                logging.debug('Using direct access grant to acquire access token.')
+                token_response = direct_access_grant_token(self.username, self.password, self.host, self.realm, self.client_id)
+
             self.__access_token = token_response['access_token']
 
         return self.__access_token
--- pycloak/test/test_auth.py
+++ pycloak/test/test_auth.py
@@ -1,5 +1,5 @@
 
-from pycloak import auth
+from pycloak import auth, admin, realm, client
 import pytest
 
 def test_auth_success(keycloak_server, admin_username, admin_password):
@@ -11,3 +11,19 @@ def test_auth_failure(keycloak_server, admin_username, admin_password):
     session = auth.AuthSession(admin_username, 'NOT' + admin_password)
     with pytest.raises(auth.AuthException, message='Invalid credentials did not cause exception'):
         access_token = session.get_access_token()
+
+def test_get_offline_token(keycloak_server, admin_username, admin_password):
+    # First, have to make sure that admin-cli has access to all roles, otherwise offline_token requests will fail
+    session = auth.AuthSession(admin_username, admin_password)
+    kc_admin = admin.Admin(session)
+    admin_cli = kc_admin.realm('master').client_id('admin-cli')
+    admin_cli['fullScopeAllowed'] = 'true'
+    kc_admin.realm('master').update_client(admin_cli)
+
+    token_response = auth.direct_access_grant_token(admin_username, admin_password, include_offline=True)
+    assert token_response.get('refresh_token') != None
+
+def test_use_offline_token(keycloak_server, admin_username, admin_password):
+    direct_access_token_response = auth.direct_access_grant_token(admin_username, admin_password, include_offline=True)
+    offline_token_response = auth.offline_token(direct_access_token_response['refresh_token'])
+    assert offline_token_response.get('access_token') != None
